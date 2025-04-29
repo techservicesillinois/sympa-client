4089453bc10a4e3811e6155ef25115b2e6bcd7eb
cmotek <cmotek1122@gmail.com>
2025-04-29 13:24:21 -0500

    Review function created in separate file, test file created for running review function
    fixed issues

---
+++ src/main/java/edu/illinois/techservices/sympa/Test.java
@@ -0,0 +1,13 @@
+package edu.illinois.techservices.sympa;
+
+public class Test {
+
+    public static void main(String[] args) 
+        {
+        
+        Main.main(new String[] {"review"});
+        
+        }
+    
+        
+    }
