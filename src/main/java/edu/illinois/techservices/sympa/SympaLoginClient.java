1a21896cda05a8b0a9f8099e25f2d055ad6a650e
pbale <pbale@illinois.edu>
2025-04-30 17:07:36 -0400

    code for getUserEmailByCookie and authenticateRemoteAppAndRun

--- src/main/java/edu/illinois/techservices/sympa/SympaLoginClient.java
+++ src/main/java/edu/illinois/techservices/sympa/SympaLoginClient.java
@@ -99,15 +99,142 @@ public class SympaLoginClient {
     }
   }
 
-  public static void authenticateRemoteAppAndRun(String cookie) {
+  /**
+   * Allow a trusted remote application to perform operations on behalf of a user
+   * without requiring that user's cookie or password. In other words, impersonate
+   * a user securely from a backend system
+   * For example: A web app or script wants to call which, del or add on behalf of
+   * a user without logging them in through the UI.
+   * 
+   * @param cookie
+   */
+  public static void authenticateRemoteAppAndRun() {
+    try {
 
+      MessageFactory messageFactory = MessageFactory.newInstance();
+      SOAPMessage soapMessage = messageFactory.createMessage();
+      SOAPPart soapPart = soapMessage.getSOAPPart();
+      SOAPEnvelope envelope = SympaClient.addNamespaceDeclaration(soapPart);
+
+      // Encode password in Base64
+      String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes());
+      SOAPBody soapBody = envelope.getBody();
+
+      SOAPElement soapElement = soapBody.addChildElement("authenticateRemoteAppAndRun", "ns", "urn:sympasoap");
+
+      System.out.println("Base64 Encoded Password: " + encodedPassword);
+
+      soapElement.addChildElement("appname").addTextNode("sampleremoteApp").addAttribute(new QName("xsi:type"),
+          "xsd:string");
+
+      soapElement.addChildElement("apppassword").addTextNode("12345").addAttribute(new QName("xsi:type"), "xsd:string");
+
+      soapElement.addChildElement("vars").addTextNode("USER_EMAIL=pbale@illinois.edu")
+          .addAttribute(new QName("xsi:type"), "xsd:string");
+
+      soapElement.addChildElement("service").addTextNode("add").addAttribute(new QName("xsi:type"), "xsd:string");
+
+      // List<String> p = new ArrayList<>();
+      String[] p = {
+          "scrumTeamB",
+          "pbale@xyz.edu",
+          "true",
+          "true" };
+
+      System.out.println("parameters size: " + p);
+      // ArrayOfString parameter
+
+      SOAPElement param5 = soapElement.addChildElement("parameters", "ns");
+      param5.addAttribute(new QName("SOAP-ENC:arrayType", "arrayType", "SOAP-ENC"), "xsd:string[" + p.length + "]");
+      param5.addAttribute(new QName("xsi:type"), "SOAP-ENC:Array");
+
+      if (p != null && p.length > 0) {
+        for (String param : p) {
+          SOAPElement item = param5.addChildElement("item");
+          item.addTextNode(param);
+          item.addAttribute(new QName("xsi:type"), "xsd:string");
+        }
+      }
+
+      String auth = email + ":" + password;
+      String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes("UTF-8"));
+      MimeHeaders headers = soapMessage.getMimeHeaders();
+
+      headers.addHeader("Authorization", encodedAuth);
+      headers.addHeader("Content-Type", "text/xml"); // application/soap+xml
+      headers.addHeader("SOAPAction", "urn:sympasoap#authenticateRemoteAppAndRun");
+      headers.addHeader("RequestMethod", "POST");
+      soapMessage.saveChanges();
+
+      SympaClient.printSOAPMessage(soapMessage);
+      System.out.println("\n");
+
+      System.out.println("\n SoapConnection.call() : \n");
+
+      SOAPMessage soapResponse = SympaClient.callSympaAPI(soapMessage);
+
+      System.out.println("\n AuthenticateAndRun Response: \n");
+      SympaClient.printSOAPMessage(soapResponse);
+
+    } catch (Exception e) {
+      System.out.println("\n THE ERROR...\n");
+      e.printStackTrace();
+    }
   }
 
   public static void casLogin(String cookie) {
 
   }
 
+  /**
+   * Return the user who sent the specific authorized session cookie to sympa
+   * server.
+   * 
+   * @param cookie
+   */
   public static void getUserEmailByCookie(String cookie) {
+    try {
 
+      MessageFactory messageFactory = MessageFactory.newInstance();
+      SOAPMessage soapMessage = messageFactory.createMessage();
+      SOAPPart soapPart = soapMessage.getSOAPPart();
+      SOAPEnvelope envelope = SympaClient.addNamespaceDeclaration(soapPart);
+
+      SOAPBody soapBody = envelope.getBody();
+
+      SOAPElement soapElement = soapBody.addChildElement("getUserEmailByCookie", "ns", "urn:sympasoap");
+
+      // Encode password in Base64
+      String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes());
+
+      System.out.println("Base64 Encoded Password: " + encodedPassword);
+
+      soapElement.addChildElement("cookie").addTextNode(cookie).addAttribute(new QName("xsi:type"),
+          "xsd:string");
+
+      String auth = email + ":" + password;
+      String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes("UTF-8"));
+      MimeHeaders headers = soapMessage.getMimeHeaders();
+
+      headers.addHeader("Authorization", encodedAuth);
+      headers.addHeader("Content-Type", "text/xml"); // application/soap+xml
+      headers.addHeader("SOAPAction", "urn:sympasoap#getUserEmailByCookie");
+      headers.addHeader("RequestMethod", "POST");
+      soapMessage.saveChanges();
+
+      SympaClient.printSOAPMessage(soapMessage);
+      System.out.println("\n");
+
+      System.out.println("\n SoapConnection.call() : \n");
+
+      SOAPMessage soapResponse = SympaClient.callSympaAPI(soapMessage);
+
+      System.out.println("\n getUserEmailByCookie Response: \n");
+      SympaClient.printSOAPMessage(soapResponse);
+
+    } catch (Exception e) {
+      System.out.println("\n THE ERROR...\n");
+      e.printStackTrace();
+    }
   }
 }
