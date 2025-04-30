33fc11cdb4fe3e72c315f8ea25e34cdcf9ca091d
cmotek <cmotek1122@gmail.com>
2025-04-30 12:18:43 -0500

    Subscribe function created in separate file, test file created for running subscribe function
    Fixing Rebase Issues.

---
+++ src/main/java/edu/illinois/techservices/sympa/Main.java
@@ -0,0 +1,51 @@
+package edu.illinois.techservices.sympa;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    String sessionCookie = null;
+    try {
+      String input = args[0];
+      System.out.println("input: " + input);
+      sessionCookie = SympaClient.loginSympa();
+      
+      if (sessionCookie != null) {
+
+        switch(input) {
+          case "getList": {
+            SympaClient.getLists(sessionCookie);
+            break;
+          }
+          case "createList": {
+            //SympaClient.createList(sessionCookie);
+            break;
+          }
+          case "getInfo":
+          {
+            SympaClient.getInfo(sessionCookie);
+            break;
+          }
+          case "review":
+          {
+            //Review.review(sessionCookie);
+            break;
+          }
+          case "subscribe":
+          {
+            //Subscribe.subscribe(sessionCookie);
+            break;
+          }
+          default:
+            System.out.println("wrong input");
+        }
+       
+      }
+      
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    
+    
+     
+  }
+}
