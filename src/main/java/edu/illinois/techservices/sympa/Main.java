0161d6229bede70918182c75335b0282a01c4fbf
Robert Stanton <rstanton@illinois.edu>
2025-05-08 15:54:01 -0500

    Initial commit of Sympa list operations (add, delete, closeList, complexLists.

--- src/main/java/edu/illinois/techservices/sympa/Main.java
+++ src/main/java/edu/illinois/techservices/sympa/Main.java
@@ -4,13 +4,20 @@ public class Main {
   
   public static void main(String[] args) {
     String sessionCookie = null;
-    try {
-      String input = args[0];
-      System.out.println("input: " + input);
+    String input;
+      try {
+        input = args[0];
+        System.out.println("input: " + input);
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("[ERROR] Please provide an argument for function call.");
+        System.out.println("  Example args: getList, createList, getInfo");
+        System.out.println("  Example Usage: -Dexec.args=\"getList\"");
+        throw new IllegalArgumentException("[ERROR] Please provide an argument for function call. See logs for details.");
+      }
+      // TODO: Validate call before logging in (use enum or something similar?)
       sessionCookie = SympaClient.loginSympa();
       
       if (sessionCookie != null) {
-
         switch(input) {
           case "getList": {
             SympaClient.getLists(sessionCookie);
@@ -25,17 +32,29 @@ public class Main {
             SympaClient.getInfo(sessionCookie);
             break;
           }
+          case "add":
+          {
+            SympaClient.add(sessionCookie);
+            break;
+          }
+          case "del":
+          {
+            SympaClient.del(sessionCookie);
+            break;
+          }
+          case "getComplexLists": {
+            SympaClient.getComplexLists(sessionCookie);
+            break;
+          }
+          case "closeList": {
+            SympaClient.closeList(sessionCookie);
+            break;
+          }
           default:
-            System.out.println("wrong input");
+            System.out.println("Invalid API call. Please provide a valid function call.");
+            System.out.println("Example args: getList, createList, getInfo");
+            System.out.println("Example Usage: -Dexec.args=\"getList\"");
+          }
         }
-       
-      }
-      
-    } catch(Exception e) {
-      e.printStackTrace();
-    }
-    
-    
-     
   }
 }
