b938efd94c807ebb1676163ff4bdf7753bddf7c6
Andy Gherna <argherna@gmail.com>
2025-05-22 13:02:20 -0500

    Add demo program for getting sympa lists.

---
+++ src/main/java/edu/illinois/techservices/sympa/demo/GetListsFromSympaDemo.java
@@ -0,0 +1,258 @@
+package edu.illinois.techservices.sympa.demo;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.xml.namespace.QName;
+
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.MimeHeaders;
+import jakarta.xml.soap.SOAPBody;
+import jakarta.xml.soap.SOAPConnection;
+import jakarta.xml.soap.SOAPConnectionFactory;
+import jakarta.xml.soap.SOAPElement;
+import jakarta.xml.soap.SOAPEnvelope;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
+import jakarta.xml.soap.SOAPPart;
+
+/**
+ * Demonstration of how to contact a Sympa server for email lists.
+ * 
+ * <P>
+ * This demonstration uses the SAAJ API for accessing the SOAP server. These
+ * system properties may be set at the command line to override default values:
+ * 
+ * <DL>
+ * <DT><CODE>sympa.url</CODE>
+ * <DD>URL for the Sympa SOAP server.
+ * <DT><CODE>sympa.username</CODE>
+ * <DD>Sympa SOAP server username.
+ * <DT><CODE>sympa.password</CODE>
+ * <DD>Sympa SOAP server password.
+ * <DL>
+ */
+public class GetListsFromSympaDemo implements Runnable {
+
+    public static final String DEFAULT_SYMPA_URL = "http://localhost:8080";
+
+    public static final String DEFAULT_SYMPA_EMAIL = "sympaemail";
+
+    public static final String DEFAULT_SYMPA_PASSWORD = "sympapassword";
+
+    private String sympaUrl = System.getProperty("sympa.url", DEFAULT_SYMPA_URL);
+
+    private String sympaEmail = System.getProperty("sympa.email", DEFAULT_SYMPA_EMAIL);
+
+    private String sympaPassword = System.getProperty("sympa.password", DEFAULT_SYMPA_PASSWORD);
+
+    private String sessionCookie = "";
+
+    private SOAPMessage sympaResponse;
+
+    /**
+     * Run the demo.
+     * 
+     * <P>
+     * A demonstration of the flow for interacting with Sympa. The program will
+     * verify the credentials and sympa server url have been set to some value, a
+     * login will be attempted, and the call to getLists will be executed. If
+     * getLists is successful, the SOAPMessage for this class will be set and can be
+     * read from a call to {@link #getSympaResponse()}.
+     */
+    @Override
+    public void run() {
+        checkCredentialsSet();
+        checkSympaUrlSet();
+        login();
+
+        // Make sure a SOAP fault wasn't returned. If the {@link #sympaResponse} isn't
+        // {@code null}, then there was a fault and this method should return
+        // immediately.
+        if (Objects.nonNull(sympaResponse)) {
+            return;
+        }
+
+        // Make sure the session cookie has been set, otherwise that's an error.
+        if (sessionCookie.length() == 0) {
+            throw new RuntimeException("Login failed, check that your credentials and url are set correctly.");
+        }
+
+        getLists();
+    }
+
+    /**
+     * Demonstrates a Sympa login.
+     * 
+     * <P>
+     * Logins must be run before execution of requested operation. This method will
+     * send the credentials and will take the session cookie from the response if
+     * successful.
+     * 
+     * <P>
+     * If the Sympa server sends a SOAP fault, the message is captured and can be
+     * accessed via calls to {@link #getSympaResponse()}. No exception will be
+     * thrown because of a SOAP fault.
+     */
+    private void login() {
+        try {
+            SOAPMessage smLogin = MessageFactory.newInstance().createMessage();
+            SOAPPart spLogin = smLogin.getSOAPPart();
+
+            // Declare SOAP and sympa namespaces
+            SOAPEnvelope senvLogin = spLogin.getEnvelope();
+            addSympaNamespaceDeclarations(senvLogin);
+
+            // Create SOAP request body
+            SOAPBody sbLogin = senvLogin.getBody();
+            SOAPElement seltLogin = sbLogin.addChildElement("login", "ns", "urn:sympasoap");
+            seltLogin.addChildElement("email").addTextNode(sympaEmail).addAttribute(new QName("xsi:type"),
+                    "xsd:string");
+            seltLogin.addChildElement("password").addTextNode(sympaPassword).addAttribute(new QName("xsi:type"),
+                    "xsd:string");
+
+            // Set MIME headers
+            MimeHeaders mhLogin = smLogin.getMimeHeaders();
+            mhLogin.addHeader("Content-Type", "text/xml");
+            mhLogin.addHeader("SOAPAction", "urn:sympasoap#login");
+            mhLogin.addHeader("RequestMethod", "POST");
+
+            smLogin.saveChanges();
+
+            // Initialize SOAP connection
+            SOAPConnectionFactory scfLogin = SOAPConnectionFactory.newInstance();
+            SOAPConnection scLogin = scfLogin.createConnection();
+
+            // Send requst and expect a response.
+            SOAPMessage smLoginResponse = scLogin.call(smLogin, sympaUrl);
+
+            // Get the session cookie that verifies authentication on functional calls.
+            SOAPBody sbLoginResponse = smLoginResponse.getSOAPBody();
+
+            // Grab Session Cookie from ResponseBody
+            if (sbLoginResponse.hasFault()) {
+                sympaResponse = smLoginResponse;
+            } else {
+                sessionCookie = sbLoginResponse.getTextContent();
+            }
+
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns Sympa Lists.
+     * 
+     * <P>
+     * This method should be called <STRONG>AFTER</STRONG> {@link #login()}.
+     */
+    private void getLists() {
+        try {
+            SOAPMessage smGetLists = MessageFactory.newInstance().createMessage();
+            SOAPPart spGetLists = smGetLists.getSOAPPart();
+
+            // Declare SOAP and sympa namespaces
+            SOAPEnvelope senvGetLists = spGetLists.getEnvelope();
+            addSympaNamespaceDeclarations(senvGetLists);
+
+            // Create SOAP request body.
+            SOAPBody sbGetLists = senvGetLists.getBody();
+            @SuppressWarnings("unused")
+            SOAPElement unused = sbGetLists.addChildElement("lists", "ns", "urn:sympasoap");
+
+            // Set MIME headers
+            MimeHeaders headers = smGetLists.getMimeHeaders();
+            headers.addHeader("Content-Type", "text/xml"); // application/soap+xml
+            headers.addHeader("SOAPAction", "urn:sympasoap#lists");
+            headers.addHeader("Cookie", "sympa_session=" + sessionCookie);
+
+            smGetLists.saveChanges();
+
+            // Initialize SOAP connection
+            SOAPConnectionFactory scfGetLists = SOAPConnectionFactory.newInstance();
+            SOAPConnection scGetLists = scfGetLists.createConnection();
+
+            // Send requst and expect a response.
+            sympaResponse = scGetLists.call(smGetLists, sympaUrl);
+
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Adds Sympa-specific namespace declarations to the SOAP envelope.
+     * 
+     * @param senv the SOAPEnvelope.
+     * @throws SOAPException if a SOAPException is thrown.
+     */
+    private void addSympaNamespaceDeclarations(SOAPEnvelope senv) throws SOAPException {
+        senv.addNamespaceDeclaration("ns", "urn:sympasoap");
+        senv.addNamespaceDeclaration("soapenc", "http://schemas.xmlsoap.org/soap/encoding/");
+        senv.addNamespaceDeclaration("soap", "http://schemas.xmlsoap.org/wsdl/soap/");
+        senv.addNamespaceDeclaration("xsd", "http://www.w3.org/2001/XMLSchema");
+        senv.addNamespaceDeclaration("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        senv.addNamespaceDeclaration("targetNamespace",
+                "https://lists-dev.techservices.illinois.edu/lists/wsdl");
+        senv.addNamespaceDeclaration("SOAP-ENC", "http://schemas.xmlsoap.org/soap/encoding/");
+        senv.setEncodingStyle("http://schemas.xmlsoap.org/soap/encoding/");
+    }
+
+    /**
+     * Checks if username and password is non null and non empty. If empty, throw an
+     * IllegalStateException with a message to add them to the code and recompile.
+     * 
+     * @throws IllegalStateException if credentials not set.
+     */
+    private void checkCredentialsSet() {
+        if (Objects.isNull(sympaEmail) || sympaEmail.isEmpty()) {
+            throw new IllegalStateException("Sympa email not set! Use -Dsympa.email system property.");
+        }
+        if (Objects.isNull(sympaPassword) || sympaPassword.isEmpty()) {
+            throw new IllegalStateException("Sympa password not set! Use -Dsympa.password system property.");
+        }
+    }
+
+    /**
+     * Checks if the sympa url is non null and non empty. If empty, throw an
+     * IllegalStateException with a message to add it to the code and recompile.
+     */
+    private void checkSympaUrlSet() {
+        if (Objects.isNull(sympaUrl) || sympaUrl.isEmpty()) {
+            throw new IllegalStateException("Sympa Url not set! Use -Dsympa.url system property.");
+        }
+    }
+
+    /**
+     * 
+     * @return SOAPMessage from Sympa or {@code null} if not set.
+     */
+    public SOAPMessage getSympaResponse() {
+        return sympaResponse;
+    }
+
+    /**
+     * The main entry point.
+     * 
+     * <P>
+     * Instantiate and run this demo program. Print the Sympa response if
+     * successful, stack trace and error message otherwise.
+     * 
+     * @param args No command line arguments are specified.
+     */
+    public static void main(String[] args) {
+        GetListsFromSympaDemo app = new GetListsFromSympaDemo();
+        app.run();
+
+        // Print the Sympa response to System.out.
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            app.getSympaResponse().writeTo(out);
+            System.out.println(new String(out.toByteArray()));
+        } catch (SOAPException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
