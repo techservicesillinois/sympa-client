81428d2b7a92faac81343f24346c8b2dc0b7c6b2
Robert Stanton <rstanton00@users.noreply.github.com>
2025-05-28 16:05:52 -0500

    Adding four separate Sympa list operations (#19)
    * Add fullReview java class with all its methods inside it.
    
    * Break out Sympa list operations into four separate files.
    
    * Correct comments and remove @suppressWarnings across the four list operations files.
    
    ---------
    
    Co-authored-by: Prasanna Bale <pbale@illinois.edu>

--- src/main/java/edu/illinois/techservices/sympa/Add.java
+++ src/main/java/edu/illinois/techservices/sympa/Add.java
@@ -1,15 +1,8 @@
 package edu.illinois.techservices.sympa;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.xml.namespace.QName;
-
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import jakarta.xml.soap.MessageFactory;
 import jakarta.xml.soap.MimeHeaders;
@@ -24,11 +17,8 @@ import jakarta.xml.soap.SOAPPart;
 import jakarta.xml.soap.SOAPFault;
 
 /**
- * 1. Demonstration of how to contact a Sympa server to list the members,
- * editors
- * and owners of the given list.
- * 2. Enumerates through the full review results and print the
- * list of users by the given permission type (subscriber/owner/editor).
+ * 1. Demonstration of how to contact a Sympa server to add a new member to
+ *    a given list.
  * <P>
  * This demonstration uses the SAAJ API for accessing the SOAP server. These
  * system properties may be set at the command line to override default values:
@@ -48,7 +38,7 @@ import jakarta.xml.soap.SOAPFault;
  * should never be deployed to a production environment where security is
  * valued.
  */
-public class FullReviewBusinessCase implements Runnable {
+public class Add implements Runnable {
 
     public static final String DEFAULT_SYMPA_URL = "http://localhost:8080/sympasoap";
 
@@ -66,33 +56,57 @@ public class FullReviewBusinessCase implements Runnable {
 
     private SOAPMessage sympaResponse;
 
-    private String permissionType;
     private String listName;
-
+    private String userEmail;
+    private String userDescription;
+    private Boolean quiet = false;
+    
     /**
      * <p>
      * This constructor validates the inputs:
      * <ul>
      * <li>{@code listName} must not be {@code null} or blank</li>
-     * <li>{@code permissionType} must not be {@code null} or blank</li>
+     * <li>{@code email} must not be {@code null} or blank</li>
+     * <li>{@code userDescription} must not be {@code null} or blank</li>
+     * <li>{@code quiet} is optional</li>
      * </ul>
      * 
      * @param args length must be the user's name; must not be {@code null} or blank
-     * @throws IllegalArgumentException if {@code listName} is null/blank or
-     *                                  {@code permissionType} is negative
+     * @throws IllegalArgumentException if {@code listName} or {@code email} 
+     *                                  or {@code userDescription} is null/blank or
+     *                                  {@code quiet} is not an accepted Boolean value
      */
-    public FullReviewBusinessCase(String[] args) {
+    public Add(String[] args) {
 
         if (args[0] == null || args[0].isEmpty()) {
-            throw new IllegalArgumentException("Please provide listName as a first argument.");
+            throw new IllegalArgumentException("Please provide a 'listName' to add to as the first argument.");
         }
         if (args[1] == null || args[1].isEmpty()) {
             throw new IllegalArgumentException(
-                    "Please provide type (subscriber, editor or owner) as  a second argument.");
+                    "Please provide the email to add as the second argument.");
+        }
+        if (args[2] == null || args[2].isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Please provide a text description of the user being added as the third argument.");
         }
 
         this.listName = args[0];
-        this.permissionType = args[1];
+        this.userEmail = args[1];
+        this.userDescription = args[2];
+        /* verify that if the user passes in a value to override the Boolean default of false,
+           that it is either 'true', '1', 'false', or '0' before we convert it to a Boolean value */
+        if (args[3] != null && !(args[3].isEmpty())) {
+            if (args[3].equalsIgnoreCase("true") || 
+                args[3].equalsIgnoreCase("1") || 
+                args[3].equalsIgnoreCase("false") || 
+                args[3].equalsIgnoreCase("0")) {
+                    this.quiet = Boolean.valueOf(args[3]);
+            }
+            else {
+                throw new IllegalArgumentException(
+                    "Please provide a valid Boolean value for the Quiet option.");
+            }
+        }
     }
 
     /**
@@ -101,12 +115,11 @@ public class FullReviewBusinessCase implements Runnable {
      * <P>
      * A demonstration of the flow for interacting with Sympa. The program will
      * verify the credentials and sympa server url have been set to some value, a
-     * login will be attempted, and the call to fullReview will be executed. If
-     * fullReview is successful, the SOAPMessage for this class will be set and can
+     * login will be attempted, and the call to addMember will be executed. If
+     * addMember is successful, the SOAPMessage for this class will be set and can
      * be read from a call to {@link #getSympaResponse()}. If there's a fault,
-     * calling
-     * the same {@link #getSympaResponse()} will make the SOAPMessage available for
-     * additional processing.
+     * calling the same {@link #getSympaResponse()} will make the SOAPMessage 
+     * available for additional processing.
      */
     @Override
     public void run() {
@@ -126,7 +139,7 @@ public class FullReviewBusinessCase implements Runnable {
             throw new RuntimeException("Login failed, check that your credentials and url are set correctly.");
         }
 
-        getFullReview();
+        addMember();
     }
 
     /**
@@ -189,128 +202,68 @@ public class FullReviewBusinessCase implements Runnable {
     }
 
     /**
-     * Demonstrates a call to the Sympa API to get members, owners and scubscribers
-     * of a list.
+     * Demonstrates a call to the Sympa API to add a member to a list.
      * 
      * <P>
      * This method should be called <STRONG>AFTER</STRONG> {@link #login()}. The
      * method {@link #getSympaResponse()} can be called to get the SOAP message that
      * was returned for additional processing after this method completes.
      */
-    private void getFullReview() {
+    private void addMember() {
         try {
             // Create SOAP Message
-            SOAPMessage smGetFullReview = MessageFactory.newInstance().createMessage();
-            SOAPPart spGetFulleReview = smGetFullReview.getSOAPPart();
+            SOAPMessage smAddMember = MessageFactory.newInstance().createMessage();
+            SOAPPart spAddMember = smAddMember.getSOAPPart();
 
             // Declare SOAP and sympa namespaces
-            SOAPEnvelope senvGetFullReview = spGetFulleReview.getEnvelope();
-            addSympaNamespaceDeclarations(senvGetFullReview);
+            SOAPEnvelope senvAddMember = spAddMember.getEnvelope();
+            addSympaNamespaceDeclarations(senvAddMember);
 
             // Create SOAP request body.
-            SOAPBody sbGetFullReview = senvGetFullReview.getBody();
-            @SuppressWarnings("seGetFullReview")
-            SOAPElement seGetFullReview = sbGetFullReview.addChildElement("fullReview", "ns", "urn:sympasoap");
-            seGetFullReview.addChildElement("list", "ns")
-                    .addTextNode(listName)
-                    .addAttribute(new QName("xsi:type"), "xsd:string");
+            SOAPBody sbAddMember = senvAddMember.getBody();
+            SOAPElement seAddMember = sbAddMember.addChildElement("add", "ns", "urn:sympasoap");
+            seAddMember.addChildElement("list", "ns")
+                .addTextNode(listName)
+                .addAttribute(new QName("xsi:type"), "xsd:string");
 
-            MimeHeaders headers = smGetFullReview.getMimeHeaders();
+            seAddMember.addChildElement("email", "ns")
+                .addTextNode(userEmail)
+                .addAttribute(new QName("xsi:type"), "xsd:string");
+
+            seAddMember.addChildElement("gecos", "ns")
+                .addTextNode(userDescription)
+                .addAttribute(new QName("xsi:type"), "xsd:string");
+
+            seAddMember.addChildElement("quiet", "ns")
+                .addTextNode(quiet.toString())
+                .addAttribute(new QName("xsi:type"), "xsd:boolean");
+
+            MimeHeaders headers = smAddMember.getMimeHeaders();
             headers.addHeader("Content-Type", "text/xml");
-            headers.addHeader("SOAPAction", "urn:sympasoap#fullReview");
+            headers.addHeader("SOAPAction", "urn:sympasoap#add");
             headers.addHeader("Cookie", "sympa_session=" + sessionCookie);
 
-            smGetFullReview.saveChanges();
+            smAddMember.saveChanges();
 
             // Initialize SOAP connection
-            SOAPConnectionFactory scfGetFullReview = SOAPConnectionFactory.newInstance();
-            SOAPConnection scGetFullReview = scfGetFullReview.createConnection();
+            SOAPConnectionFactory scfAddMember = SOAPConnectionFactory.newInstance();
+            SOAPConnection scAddMember = scfAddMember.createConnection();
 
             // Send requst and expect a response. This could be a SOAP fault if the server
             // returns one. Most of the time, expect the payload to be the mailing lists.
-            sympaResponse = scGetFullReview.call(smGetFullReview, sympaUrl);
+            sympaResponse = scAddMember.call(smAddMember, sympaUrl);
 
             // Create a SOAP connection
             SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
             SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
             // Call Sympa API by passing SOAPMessage
-            SOAPMessage fullreview = soapConnection.call(smGetFullReview, sympaUrl);
+            SOAPMessage addmember = soapConnection.call(smAddMember, sympaUrl);
 
             soapConnection.close();
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    /**
-     * Calculates the list of users based on the permission type picked.(subscriber,
-     * editor or owner)
-     * 
-     * <P>
-     * This method body is specific to only fullReview. This is generated by
-     * considering the xml output is in certain format.
-     */
-    private void enumerateListByType() throws Exception {
-
-        int count = 0;
-        List<String> emailList = new ArrayList<>();
-
-        SOAPBody responseBody = getSympaResponse().getSOAPBody();
-
-        if (responseBody.hasFault()) {
-            SOAPFault fault = responseBody.getFault();
-            System.err.println("SOAP Fault: " + fault.getFaultString());
-            // TODO: throw an exception
-        }
-
-        NodeList returnNodes = responseBody.getElementsByTagName("return");
-        if (returnNodes.getLength() == 0) {
-            System.out.println("No <return> element found.");
-        }
-
-        if ("subscriber".equalsIgnoreCase(permissionType)) {
-            permissionType = "isSubscriber";
-        } else if ("owner".equalsIgnoreCase(permissionType)) {
-            permissionType = "isOwner";
-        } else {
-            permissionType = "isEditor";
-        }
-
-        Node returnNode = returnNodes.item(0);
-        NodeList items = returnNode.getChildNodes();
-        Boolean flag = false;
-        for (int i = 0; i < items.getLength(); i++) {
-            Node item = items.item(i);
-            String email = "";
-            if (item.getNodeType() == Node.ELEMENT_NODE && "item".equals(item.getLocalName())) {
-
-                NodeList fields = item.getChildNodes();
-
-                for (int j = 0; j < fields.getLength(); j++) {
-                    Node field = fields.item(j);
-                    String fieldValue = field.getTextContent() != null ? field.getTextContent().trim() : "";
-
-                    if (field.getNodeType() == Node.ELEMENT_NODE) {
-                        if ("email".equalsIgnoreCase(field.getLocalName())) {
-                            email = fieldValue;
-                        }
-                        if (permissionType.equals(field.getLocalName()) && "true".equalsIgnoreCase(fieldValue)) {
-                            flag = true;
-                        }
-                        if (!"".equals(email) && email != null && flag) {
-                            System.out.println(email);
-
-                            // Reset the 'flag' and 'email' once we find the necessary type(i.e.,
-                            // editor/owner/subscriber) so the values that are
-                            // set are not repeated in the next iteration.
-                            flag = false;
-                            email = "";
-                        }
-                    }
-                }
-            }
         }
     }
 
@@ -374,23 +327,15 @@ public class FullReviewBusinessCase implements Runnable {
      * Instantiate and run this demo program. Print the Sympa response if
      * successful, stack trace and error message otherwise.
      * 
-     * @param args This program takes two command line arguments
-     *             1. listName - name of the list
-     *             2. permissionType - subscriber, editor or owner
+     * @param args This program doesn't take any command line arguments.
      */
     public static void main(String[] args) {
-        if (args.length !=2) {
-            System.err.println(
-                    "Usage: mvn -q exec:java -Dexec.mainClass=edu.illinois.techservices.sympa.FullReviewBusinessCase -Dexec:args=<listName> <permissionType>");
-            System.exit(0);
-        }
-        FullReviewBusinessCase fullReview = new FullReviewBusinessCase(args);
-        fullReview.run();
+        Add addUser = new Add(args);
+        addUser.run();
 
         try {
-            // Enumerate through sympaResponse object to print list of
-            // subscribers, owners or editors.
-            fullReview.enumerateListByType();
+            // print Sympa response
+            addUser.getSympaResponse();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/edu/illinois/techservices/sympa/ComplexLists.java
+++ src/main/java/edu/illinois/techservices/sympa/ComplexLists.java
@@ -1,15 +1,8 @@
 package edu.illinois.techservices.sympa;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.xml.namespace.QName;
-
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import jakarta.xml.soap.MessageFactory;
 import jakarta.xml.soap.MimeHeaders;
@@ -24,11 +17,8 @@ import jakarta.xml.soap.SOAPPart;
 import jakarta.xml.soap.SOAPFault;
 
 /**
- * 1. Demonstration of how to contact a Sympa server to list the members,
- * editors
- * and owners of the given list.
- * 2. Enumerates through the full review results and print the
- * list of users by the given permission type (subscriber/owner/editor).
+ * 1. Demonstration of how to contact a Sympa server to delete a member from
+ *    a given list.
  * <P>
  * This demonstration uses the SAAJ API for accessing the SOAP server. These
  * system properties may be set at the command line to override default values:
@@ -48,7 +38,7 @@ import jakarta.xml.soap.SOAPFault;
  * should never be deployed to a production environment where security is
  * valued.
  */
-public class FullReviewBusinessCase implements Runnable {
+public class ComplexLists implements Runnable {
 
     public static final String DEFAULT_SYMPA_URL = "http://localhost:8080/sympasoap";
 
@@ -66,47 +56,17 @@ public class FullReviewBusinessCase implements Runnable {
 
     private SOAPMessage sympaResponse;
 
-    private String permissionType;
-    private String listName;
-
-    /**
-     * <p>
-     * This constructor validates the inputs:
-     * <ul>
-     * <li>{@code listName} must not be {@code null} or blank</li>
-     * <li>{@code permissionType} must not be {@code null} or blank</li>
-     * </ul>
-     * 
-     * @param args length must be the user's name; must not be {@code null} or blank
-     * @throws IllegalArgumentException if {@code listName} is null/blank or
-     *                                  {@code permissionType} is negative
-     */
-    public FullReviewBusinessCase(String[] args) {
-
-        if (args[0] == null || args[0].isEmpty()) {
-            throw new IllegalArgumentException("Please provide listName as a first argument.");
-        }
-        if (args[1] == null || args[1].isEmpty()) {
-            throw new IllegalArgumentException(
-                    "Please provide type (subscriber, editor or owner) as  a second argument.");
-        }
-
-        this.listName = args[0];
-        this.permissionType = args[1];
-    }
-
     /**
      * Run the demo.
      * 
      * <P>
      * A demonstration of the flow for interacting with Sympa. The program will
      * verify the credentials and sympa server url have been set to some value, a
-     * login will be attempted, and the call to fullReview will be executed. If
-     * fullReview is successful, the SOAPMessage for this class will be set and can
+     * login will be attempted, and the call to ComplexLists will be executed. If
+     * ComplexLists is successful, the SOAPMessage for this class will be set and can
      * be read from a call to {@link #getSympaResponse()}. If there's a fault,
-     * calling
-     * the same {@link #getSympaResponse()} will make the SOAPMessage available for
-     * additional processing.
+     * calling the same {@link #getSympaResponse()} will make the SOAPMessage 
+     * available for additional processing.
      */
     @Override
     public void run() {
@@ -126,7 +86,7 @@ public class FullReviewBusinessCase implements Runnable {
             throw new RuntimeException("Login failed, check that your credentials and url are set correctly.");
         }
 
-        getFullReview();
+        getComplexLists();
     }
 
     /**
@@ -189,128 +149,53 @@ public class FullReviewBusinessCase implements Runnable {
     }
 
     /**
-     * Demonstrates a call to the Sympa API to get members, owners and scubscribers
-     * of a list.
+     * Demonstrates a call to the Sympa API to retrieve complex lists.
      * 
      * <P>
      * This method should be called <STRONG>AFTER</STRONG> {@link #login()}. The
      * method {@link #getSympaResponse()} can be called to get the SOAP message that
      * was returned for additional processing after this method completes.
      */
-    private void getFullReview() {
+    private void getComplexLists() {
         try {
             // Create SOAP Message
-            SOAPMessage smGetFullReview = MessageFactory.newInstance().createMessage();
-            SOAPPart spGetFulleReview = smGetFullReview.getSOAPPart();
+            SOAPMessage smComplexLists = MessageFactory.newInstance().createMessage();
+            SOAPPart spComplexLists = smComplexLists.getSOAPPart();
 
             // Declare SOAP and sympa namespaces
-            SOAPEnvelope senvGetFullReview = spGetFulleReview.getEnvelope();
-            addSympaNamespaceDeclarations(senvGetFullReview);
+            SOAPEnvelope senvComplexLists = spComplexLists.getEnvelope();
+            addSympaNamespaceDeclarations(senvComplexLists);
 
             // Create SOAP request body.
-            SOAPBody sbGetFullReview = senvGetFullReview.getBody();
-            @SuppressWarnings("seGetFullReview")
-            SOAPElement seGetFullReview = sbGetFullReview.addChildElement("fullReview", "ns", "urn:sympasoap");
-            seGetFullReview.addChildElement("list", "ns")
-                    .addTextNode(listName)
-                    .addAttribute(new QName("xsi:type"), "xsd:string");
-
-            MimeHeaders headers = smGetFullReview.getMimeHeaders();
+            SOAPBody sbComplexLists = senvComplexLists.getBody();
+            SOAPElement seComplexLists = sbComplexLists.addChildElement("complexLists", "ns", "urn:sympasoap");
+            
+            MimeHeaders headers = smComplexLists.getMimeHeaders();
             headers.addHeader("Content-Type", "text/xml");
-            headers.addHeader("SOAPAction", "urn:sympasoap#fullReview");
+            headers.addHeader("SOAPAction", "urn:sympasoap#del");
             headers.addHeader("Cookie", "sympa_session=" + sessionCookie);
 
-            smGetFullReview.saveChanges();
+            smComplexLists.saveChanges();
 
             // Initialize SOAP connection
-            SOAPConnectionFactory scfGetFullReview = SOAPConnectionFactory.newInstance();
-            SOAPConnection scGetFullReview = scfGetFullReview.createConnection();
+            SOAPConnectionFactory scfComplexLists = SOAPConnectionFactory.newInstance();
+            SOAPConnection scComplexLists = scfComplexLists.createConnection();
 
             // Send requst and expect a response. This could be a SOAP fault if the server
             // returns one. Most of the time, expect the payload to be the mailing lists.
-            sympaResponse = scGetFullReview.call(smGetFullReview, sympaUrl);
+            sympaResponse = scComplexLists.call(smComplexLists, sympaUrl);
 
             // Create a SOAP connection
             SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
             SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
             // Call Sympa API by passing SOAPMessage
-            SOAPMessage fullreview = soapConnection.call(smGetFullReview, sympaUrl);
+            SOAPMessage ComplexLists = soapConnection.call(smComplexLists, sympaUrl);
 
             soapConnection.close();
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    /**
-     * Calculates the list of users based on the permission type picked.(subscriber,
-     * editor or owner)
-     * 
-     * <P>
-     * This method body is specific to only fullReview. This is generated by
-     * considering the xml output is in certain format.
-     */
-    private void enumerateListByType() throws Exception {
-
-        int count = 0;
-        List<String> emailList = new ArrayList<>();
-
-        SOAPBody responseBody = getSympaResponse().getSOAPBody();
-
-        if (responseBody.hasFault()) {
-            SOAPFault fault = responseBody.getFault();
-            System.err.println("SOAP Fault: " + fault.getFaultString());
-            // TODO: throw an exception
-        }
-
-        NodeList returnNodes = responseBody.getElementsByTagName("return");
-        if (returnNodes.getLength() == 0) {
-            System.out.println("No <return> element found.");
-        }
-
-        if ("subscriber".equalsIgnoreCase(permissionType)) {
-            permissionType = "isSubscriber";
-        } else if ("owner".equalsIgnoreCase(permissionType)) {
-            permissionType = "isOwner";
-        } else {
-            permissionType = "isEditor";
-        }
-
-        Node returnNode = returnNodes.item(0);
-        NodeList items = returnNode.getChildNodes();
-        Boolean flag = false;
-        for (int i = 0; i < items.getLength(); i++) {
-            Node item = items.item(i);
-            String email = "";
-            if (item.getNodeType() == Node.ELEMENT_NODE && "item".equals(item.getLocalName())) {
-
-                NodeList fields = item.getChildNodes();
-
-                for (int j = 0; j < fields.getLength(); j++) {
-                    Node field = fields.item(j);
-                    String fieldValue = field.getTextContent() != null ? field.getTextContent().trim() : "";
-
-                    if (field.getNodeType() == Node.ELEMENT_NODE) {
-                        if ("email".equalsIgnoreCase(field.getLocalName())) {
-                            email = fieldValue;
-                        }
-                        if (permissionType.equals(field.getLocalName()) && "true".equalsIgnoreCase(fieldValue)) {
-                            flag = true;
-                        }
-                        if (!"".equals(email) && email != null && flag) {
-                            System.out.println(email);
-
-                            // Reset the 'flag' and 'email' once we find the necessary type(i.e.,
-                            // editor/owner/subscriber) so the values that are
-                            // set are not repeated in the next iteration.
-                            flag = false;
-                            email = "";
-                        }
-                    }
-                }
-            }
         }
     }
 
@@ -374,23 +259,15 @@ public class FullReviewBusinessCase implements Runnable {
      * Instantiate and run this demo program. Print the Sympa response if
      * successful, stack trace and error message otherwise.
      * 
-     * @param args This program takes two command line arguments
-     *             1. listName - name of the list
-     *             2. permissionType - subscriber, editor or owner
+     * @param args This program doesn't take any command line arguments.
      */
     public static void main(String[] args) {
-        if (args.length !=2) {
-            System.err.println(
-                    "Usage: mvn -q exec:java -Dexec.mainClass=edu.illinois.techservices.sympa.FullReviewBusinessCase -Dexec:args=<listName> <permissionType>");
-            System.exit(0);
-        }
-        FullReviewBusinessCase fullReview = new FullReviewBusinessCase(args);
-        fullReview.run();
+        Delete deleteUser = new Delete(args);
+        deleteUser.run();
 
         try {
-            // Enumerate through sympaResponse object to print list of
-            // subscribers, owners or editors.
-            fullReview.enumerateListByType();
+            // print Sympa response
+            deleteUser.getSympaResponse();
         } catch (Exception e) {
             e.printStackTrace();
         }
